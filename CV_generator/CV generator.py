07c596eca26029bb79f75982dc0b650b469366c3
maciejguc <maciej.gucc@gmail.com>
2021-04-01 21:06:04 +0200

    CV generator cleanup #1

--- CV_generator/CV generator.py
+++ CV_generator/CV generator.py
@@ -1,61 +1,47 @@
 import json
-with open('data.json') as file:
-  data = json.load(file)
+
+with open("data.json") as file:
+    data = json.load(file)
 
 color_choice = input("Provide color choice (RED/BLUE): ")
 if color_choice.upper() == "RED":
-    line_color = "rgb(220, 20, 60)"
+    color_class = "red"
 
 elif color_choice.upper() == "BLUE":
-    line_color = "rgb(62, 168, 255)"
+    color_class = "blue"
 
 else:
-    print("Instructions unclear. Setting line color to default.")
-    line_color = "rgb(62, 168, 255)"
+    print("Instructions unclear. Setting color to default.")
+    color_class = "blue"
 
-f = open('style.css', 'a')
-css = f"""
-    border-top:"""+line_color+"""3px solid;}
+f = open("CV.html", "w")
 
-.division {
-    padding-top: 10px;
-    border-top: """+line_color+"""3px solid;}"""
-
-f.write(css)
-f.close
-
-f = open('CV.html', 'w')
-
-head = (f"""<html lang="en">
+head = f"""<html lang="en">
   <head>
     <meta charset="UTF-8" />
     <title>CV generator</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
-  </head> """)
+  </head> """
 
-
-sidebar = (f"""<body>
+sidebar = f"""<body>
     <div id="sidebar">
-        <image>
             <div class="profile">
             <img src="{data["picture"]}" alt="Couldn't locate image">
-            </div>
-        </image> """)
+            </div>"""
 
-contact = (f"""<contact>
-            <div class="contact red">
-                <h3>CONTACT</h3>
-                <ul>
-                    <li id="ln"><a href="#">{data["name"]}</a><li>
-                    <li id="phone">{data["phone"]}</li>
-                    <li id="mail">{data["email"]}</li>
-                    <li id="address">{data["address"]}</li>
-                </ul>
-            </div>
-        </contact>
-    </div> """)
+contact = f"""
+    <div class="contact {color_class}">
+        <h3>CONTACT</h3>
+            <ul>
+                <li id="ln"><a href="#">{data["name"]}</a><li>
+                <li id="phone">{data["phone"]}</li>
+                <li id="mail">{data["email"]}</li>
+                <li id="address">{data["address"]}</li>
+            </ul>
+        </div>
+    </div> """
 
-main_info = (f"""<div id="main-info">
+main_info = f"""<div id="main-info">
         <header class="intro">
             <h1>{data["name"]}</h1>
                 <div class="paragraph">
@@ -66,7 +52,7 @@ main_info = (f"""<div id="main-info">
                         <li><p>{data["about"]}</p></li>
                     </ul>
                 </div>
-        </header> """)
+        </header> """
 
 # education = (f"""<experience>
 #             <div class="division">
@@ -77,56 +63,53 @@ main_info = (f"""<div id="main-info">
 #             </div>
 #         </experience> """)
 
-skills = (f"""<skills>
-            <div class="division red">
+skills = f"""
+            <div class="division {color_class}">
             <h3>SKILLS</h3>
                 <div class="paragraph">
-                    <p class="list-title">Skills:</p> <ul> """)
+                    <p class="list-title">Skills:</p> <ul> """
+
 
 def list_skills():
     skill_list = ""
 
     for element in data["skills"]:
         grade = ""
-        for i in range((int(element['grade']))):
+        for i in range((int(element["grade"]))):
             grade += "*"
-        skill_list += (f"<li>{element['skill']} - {grade}</li>")
+        skill_list += f"<li>{element['skill']} - {grade}</li>"
     return skill_list
+
 
 skills += list_skills()
 
-skills += (f""" </ul>
+skills += f""" </ul>
                 </div>
-            </div>
-        </skills>""")
+            </div> """
 
-education = (f"""<education>
-            <div class="division">
+education = f"""
+            <div class="division {color_class}">
             <h3>EDUCATION</h3>
-                <div class="paragraph"><p>""")
+                <div class="paragraph"><p>"""
+
 
 def education_list():
     school_list = ""
     for element in data["education"]:
-        school_list += (f"<br><br>{element['name']}<br>{element['time']}")
+        school_list += f"<br><br>{element['name']}<br>{element['time']}"
     return school_list
+
 
 education += education_list()
 
-education += (f"""</p>
+education += f"""</p>
               </div>
             </div>
-        </education>
       </div>
     </div>
   </body>
-</html> """)
+</html> """
 
-f.write(head)
-f.write(sidebar)
-f.write(contact)
-f.write(main_info)
-f.write(skills)
-f.write(education)
-
+html = head + sidebar + contact + main_info + skills + education
+f.write(html)
 f.close()
